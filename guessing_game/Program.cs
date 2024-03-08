a8d04ae00bc0d178a56361da24d7d6597b5a33fa
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-08 02:40:19 +0100

    geusWord function added

---
+++ guessing_game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+class MyApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = "";
+            geusWord(input);
+        }
+
+        static void geusWord( string input)
+        {
+            string secretWord = "Google";
+            while (input != secretWord)
+            {
+                Console.Write("Guess the secret word: ");
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine("Wel done");
+        }
+    }
+}

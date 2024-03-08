542fe2912f18fd1ff42619af336222181b44ed49
DaviesNyantakyi <davies.nyantakyi@outlook.col>
2024-03-08 02:59:36 +0100

    score tracker added

--- guessing_game/Program.cs
+++ guessing_game/Program.cs
@@ -9,18 +9,37 @@ class MyApp
         {
             string input = "";
             geusWord(input);
+           
         }
 
-        static void geusWord( string input)
+        static void geusWord(string input)
         {
+            int geuses = 0;
             string secretWord = "Google";
-            while (input != secretWord)
+            while (input != secretWord && geuses < 4)
             {
-                Console.Write("Guess the secret word: ");
-                input = Console.ReadLine();
 
+                if (geuses < 4)
+                {
+                    Console.Write("Guess the secret word: ");
+                    input = Console.ReadLine();
+                    geuses++;
+                }
+                
             }
-            Console.WriteLine("Wel done");
+
+            if(geuses < 4)
+            {
+                Console.WriteLine("Wel done");
+            }
+            else
+            {
+                Console.WriteLine("You lose");
+            }   
+
+
         }
+
+
     }
 }
--- guessing_game/obj/Debug/net8.0/guessing_game.AssemblyInfo.cs
+++ guessing_game/obj/Debug/net8.0/guessing_game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guessing_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d04ae00bc0d178a56361da24d7d6597b5a33fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("guessing_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guessing_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
